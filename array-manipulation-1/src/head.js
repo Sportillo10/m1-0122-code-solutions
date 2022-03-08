aadc3573015684db7f0692b3cb7d61033a56ea47
Steve Portillo <steve.prtll10@gmail.com>
2022-03-07 19:47:03 -0800

    Completed DOM Querying.

--- array-manipulation-1/src/head.js
+++ array-manipulation-1/src/head.js
@@ -1,9 +1,6 @@
 /* exported head */
 // I need to define a function named head, which has one parameter 'array'.
-// I then need to define a variable named results and assign it an empty array.
-// I will use a for loop to check the property in the parameter.
-// In the loop, I will push the the values to the empty array.
-// When the loop is complete, I will return the values array.
+// I will then need to return the array and index it to 0.
 function head(array) {
   return array[0];
 }
--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,4 +1,10 @@
 /* exported last */
+// 1. I need to define the function and label it last with one parameter set to array.
+// 2. I will then set a variable results equalling an array literal set to 0.
+// 3. I then will return the variable results.
 function last(array) {
   return array[array.length - 1];
 }
+
+// 1. I needed to get rid of my variable and return the array and in a array
+// I set the parameter array and gave it the method length and subtracted 1.
