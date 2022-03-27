6578e72054f48d3fee6ee06a3c79b783f316e065
Steve Portillo <steve.prtll10@gmail.com>
2022-03-27 15:09:39 -0700

    Completed String Manipulation 1.

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -1,1 +1,16 @@
 /* exported capitalize */
+function capitalize(word) {
+  var cap = word[0].toUpperCase();
+  var str = word.split('');
+  var low = str.slice(1).join('');
+  cap += low.toLowerCase();
+  return cap;
+}
+
+// Pseudo Code
+// define function named capitalize which takes single parameter 'word'
+// target the first letter of the 'word' and uppercase it and assign to variable 'cap'
+// define variable 'str' and assign it to the value of the 'word' which we turned into an array
+// delete the first letter of the now array 'word' and convert into string, assign to variable 'low'
+// turn the variable 'low' to lowercase and add it to the variable 'cap'
+// return 'cap'
--- string-manipulation-1/src/is-lower-cased.js
+++ string-manipulation-1/src/is-lower-cased.js
@@ -1,1 +1,10 @@
 /* exported isLowerCased */
+function isLowerCased(word) {
+  var arr = word.split('');
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i].toUpperCase() && arr[i] !== '-') {
+      return false;
+    }
+  }
+  return true;
+}
