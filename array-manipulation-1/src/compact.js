8d04fd99f13003f89cb507bc24bb9e932091263b
Steve Portillo <steve.prtll10@gmail.com>
2022-03-27 14:57:43 -0700

    Completed Array Manipulation 1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,12 +1,17 @@
 /* exported compact */
 function compact(array) {
-  var results = [];
+  var arr = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === false || array[i] === null || array[i] === 0 || array[i] === undefined || Number.isNaN(array[i]) || array[i] === '') {
-      continue;
+    if (array[i]) {
+      arr.push(array[i]);
     }
-
-    results.push(array[i]);
   }
-  return results;
+  return arr;
 }
+
+//  Pseudo Code
+//  define a function 'compact' which takes single parameter 'array'
+//  for loop which loops through the array, starting at index 0
+//  elements we want to omit from the arrays are 'falsy' values
+//  create if statement to check array[i] is truthy if so push to arr
+//  return 'arr'
--- array-manipulation-1/src/head.js
+++ array-manipulation-1/src/head.js
@@ -1,6 +1,4 @@
 /* exported head */
-// I need to define a function named head, which has one parameter 'array'.
-// I will then need to return the array and index it to 0.
 function head(array) {
   return array[0];
 }
